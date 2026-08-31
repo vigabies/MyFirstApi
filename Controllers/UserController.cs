33078bd99dbdf5e39ac2054b24df400fb2dda954
Gabriela Silva <vigabiesdivision@gmail.com>
2026-08-31 10:59:48 -0300

    Configurações Iniciais V1

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+
+[Route("api/[controller]")] //atributo obrigatório para definir a rota do controller
+[ApiController] //significa que é um controller de API, habilitando recursos como validação automática de modelo e respostas automáticas para erros de validação
+
+public class UserController : ControllerBase
+{
+}
